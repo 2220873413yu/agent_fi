4f5cf79b01f14976914a411f66c8bc1850d25278
1760602 <17606020554@163.com>
2026-05-18 21:12:57 +0800

    polymarket代码提交

--- xms-ui/src/views/xms/polymarketMarket/index.vue
+++ xms-ui/src/views/xms/polymarketMarket/index.vue
@@ -1,17 +1,17 @@
-﻿<template>
+<template>
   <div class="app-container">
     <el-form v-show="showSearch" ref="queryForm" :model="queryParams" size="small" :inline="true" label-width="90px">
-      <el-form-item label="甯傚満slug" prop="marketSlug">
-        <el-input v-model="queryParams.marketSlug" placeholder="璇疯緭鍏ュ競鍦簊lug" clearable @keyup.enter.native="handleQuery" />
+      <el-form-item label="市场slug" prop="marketSlug">
+        <el-input v-model="queryParams.marketSlug" placeholder="请输入市场slug" clearable @keyup.enter.native="handleQuery" />
       </el-form-item>
-      <el-form-item label="浜嬩欢鏍囬" prop="eventTitle">
-        <el-input v-model="queryParams.eventTitle" placeholder="璇疯緭鍏ヤ簨浠舵爣棰? clearable @keyup.enter.native="handleQuery" />
+      <el-form-item label="事件标题" prop="eventTitle">
+        <el-input v-model="queryParams.eventTitle" placeholder="请输入事件标题" clearable @keyup.enter.native="handleQuery" />
       </el-form-item>
-      <el-form-item label="甯傚満闂" prop="marketQuestion">
-        <el-input v-model="queryParams.marketQuestion" placeholder="璇疯緭鍏ュ競鍦洪棶棰? clearable @keyup.enter.native="handleQuery" />
+      <el-form-item label="市场问题" prop="marketQuestion">
+        <el-input v-model="queryParams.marketQuestion" placeholder="请输入市场问题" clearable @keyup.enter.native="handleQuery" />
       </el-form-item>
-      <el-form-item label="鐘舵€? prop="status">
-        <el-select v-model="queryParams.status" placeholder="璇烽€夋嫨鐘舵€? clearable>
+      <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择状态" clearable>
           <el-option
             v-for="dict in dict.type.t_polymarket_market_status"
             :key="dict.value"
@@ -21,8 +21,8 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">鎼滅储</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">閲嶇疆</el-button>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
 
@@ -35,38 +35,38 @@
           icon="el-icon-download"
           size="mini"
           @click="handleExport"
-        >瀵煎嚭</el-button>
+        >导出</el-button>
       </el-col>
       <right-toolbar :show-search.sync="showSearch" @queryTable="getList" />
     </el-row>
 
     <el-table v-loading="loading" :data="polymarketMarketList">
-      <el-table-column label="甯傚満" align="left" prop="marketQuestion" min-width="260" show-overflow-tooltip />
-      <el-table-column label="甯傚満slug" align="center" prop="marketSlug" width="220" show-overflow-tooltip />
-      <el-table-column label="鐘舵€? align="center" prop="status" width="110">
+      <el-table-column label="市场" align="left" prop="marketQuestion" min-width="260" show-overflow-tooltip />
+      <el-table-column label="市场slug" align="center" prop="marketSlug" width="220" show-overflow-tooltip />
+      <el-table-column label="状态" align="center" prop="status" width="110">
         <template slot-scope="scope">
           <dict-tag :options="dict.type.t_polymarket_market_status" :value="scope.row.status" />
         </template>
       </el-table-column>
-      <el-table-column label="涓嬪崟娆℃暟" align="center" prop="orderCount" width="90" />
-      <el-table-column label="鎬籄FI" align="center" prop="totalAfiAmount" width="120" />
-      <el-table-column label="鎬绘墜缁垂AFI" align="center" prop="totalFeeAfiAmount" width="130" />
-      <el-table-column label="鎬籙SDT" align="center" prop="totalUsdtAmount" width="120" />
-      <el-table-column label="鎬讳唤棰? align="center" prop="totalShareAmount" width="120" />
-      <el-table-column label="鎬诲厬浠楿SDT绛夊€? align="center" prop="totalPayoutUsdtAmount" width="130" />
-      <el-table-column label="鎬诲厬浠楢FI" align="center" prop="totalPayoutAfiAmount" width="120" />
-      <el-table-column label="璧㈠" align="center" prop="resultOutcomeName" width="120" />
-      <el-table-column label="缁撴潫鏃堕棿" align="center" prop="endTime" width="160">
+      <el-table-column label="下单次数" align="center" prop="orderCount" width="90" />
+      <el-table-column label="总AFI" align="center" prop="totalAfiAmount" width="120" />
+      <el-table-column label="总手续费AFI" align="center" prop="totalFeeAfiAmount" width="130" />
+      <el-table-column label="总USDT" align="center" prop="totalUsdtAmount" width="120" />
+      <el-table-column label="总份额" align="center" prop="totalShareAmount" width="120" />
+      <el-table-column label="总兑付USDT等值" align="center" prop="totalPayoutUsdtAmount" width="130" />
+      <el-table-column label="总兑付AFI" align="center" prop="totalPayoutAfiAmount" width="120" />
+      <el-table-column label="赢家" align="center" prop="resultOutcomeName" width="120" />
+      <el-table-column label="结束时间" align="center" prop="endTime" width="160">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.endTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="涓婃妫€鏌? align="center" prop="lastCheckTime" width="160">
+      <el-table-column label="上次检查" align="center" prop="lastCheckTime" width="160">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.lastCheckTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="鎿嶄綔" align="center" class-name="small-padding fixed-width" width="170">
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="170">
         <template slot-scope="scope">
           <el-button
             v-hasPermi="['xms:polymarketMarket:query']"
@@ -74,7 +74,7 @@
             type="text"
             icon="el-icon-view"
             @click="handleDetail(scope.row)"
-          >璇︽儏</el-button>
+          >详情</el-button>
           <el-button
             v-hasPermi="['xms:polymarketMarket:edit']"
             size="mini"
@@ -82,13 +82,13 @@
             icon="el-icon-check"
             :disabled="scope.row.status !== 0"
             @click="handleSettle(scope.row)"
-          >缁撶畻</el-button>
+          >结算</el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       :page.sync="queryParams.pageNum"
       :limit.sync="queryParams.pageSize"
@@ -97,39 +97,39 @@
 
     <el-dialog :title="title" :visible.sync="open" width="760px" append-to-body>
       <el-form ref="form" :model="form" label-width="120px">
-        <el-form-item label="甯傚満slug">
+        <el-form-item label="市场slug">
           <el-input v-model="form.marketSlug" disabled />
         </el-form-item>
-        <el-form-item label="甯傚満闂">
+        <el-form-item label="市场问题">
           <el-input v-model="form.marketQuestion" disabled />
         </el-form-item>
-        <el-form-item label="鐘舵€?>
+        <el-form-item label="状态">
           <dict-tag :options="dict.type.t_polymarket_market_status" :value="form.status" />
         </el-form-item>
-        <el-form-item label="UMA鐘舵€?>
+        <el-form-item label="UMA状态">
           <el-input v-model="form.umaResolutionStatus" disabled />
         </el-form-item>
-        <el-form-item label="璧㈠">
+        <el-form-item label="赢家">
           <el-input v-model="form.resultOutcomeName" disabled />
         </el-form-item>
         <el-form-item label="总手续费AFI">
           <el-input v-model="form.totalFeeAfiAmount" disabled />
         </el-form-item>
-        <el-form-item label="鎬诲厬浠楿SDT绛夊€?>
+        <el-form-item label="总兑付USDT等值">
           <el-input v-model="form.totalPayoutUsdtAmount" disabled />
         </el-form-item>
-        <el-form-item label="鎬诲厬浠楢FI">
+        <el-form-item label="总兑付AFI">
           <el-input v-model="form.totalPayoutAfiAmount" disabled />
         </el-form-item>
-        <el-form-item label="澶囨敞">
+        <el-form-item label="备注">
           <el-input v-model="form.remark" type="textarea" :rows="3" disabled />
         </el-form-item>
-        <el-form-item label="甯傚満蹇収">
+        <el-form-item label="市场快照">
           <el-input v-model="form.marketSnapshotJson" type="textarea" :rows="8" disabled />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="cancel">鍏?闂?/el-button>
+        <el-button @click="cancel">关闭</el-button>
       </div>
     </el-dialog>
   </div>
@@ -187,13 +187,13 @@ export default {
     handleDetail(row) {
       getPolymarketMarket(row.id).then(response => {
         this.form = response.data
-        this.title = 'Polymarket甯傚満璇︽儏'
+        this.title = 'Polymarket市场详情'
         this.open = true
       })
     },
     handleSettle(row) {
       settlePolymarketMarket(row.marketSlug).then(response => {
-        this.$modal.msgSuccess(response.data ? '宸茶Е鍙戠粨绠? : '甯傚満鏈繘鍏ョ粨绠?)
+        this.$modal.msgSuccess(response.data ? '已触发结算' : '市场未进入结算')
         this.getList()
       })
     },
